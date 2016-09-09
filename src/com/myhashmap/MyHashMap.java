08b8bf0c2ec8e96c67fc2ca2851bb42dbcd28cd9
seekdestiny <jifeiqian@gmail.com>
2016-09-08 21:56:22 -0700

    define public static final variables, private variables and private/public API.

--- src/com/myhashmap/MyHashMap.java
+++ src/com/myhashmap/MyHashMap.java
@@ -1,7 +1,26 @@
 package com.myhashmap;
 
-public class MyHashMap {
-	static class Entry<K, V> {
+/**
+ *
+ * @author jifeiqian
+ * A hashtable implementation of map and generic type is provided.
+ * 
+ * supported operations:
+ * size()
+ * isEmpty()
+ * clear()
+ * put(K key, V value)
+ * get(K key)
+ * containsKey(K key)
+ * containsValue(V value)// check if the desired values is in the map. O(n)
+ * remove(K key)
+ */
+
+public class MyHashMap<K, V> {
+	// Node is a static class of MyHashMap, since it is:
+	// very closely bonded to MyHashMap class.
+	// we probably need to access this class outside from MyHashMap class.
+	public static class Entry<K, V> {
 		final K key;
 		V value;
 		Entry<K, V> next;
@@ -23,9 +42,22 @@ public class MyHashMap {
 		}	
 	}
 	
+	//static final variable are global constants.
+	public static final int DEFAULT_CAPACITY = 16;
+	public static final float DEFAULT_LOAD_FACTOR = 0.75f;
+	
 	private Entry<K, V>[] array;
-	private int size;
-	private int loaffactor;
+	private int size; //how many key-value pairs are actually stored in the HashMap.
+	private float loadFactor; //determine when to rehash.
+	
+	public MyHashMap() {
+		this(DEFAULT_CAPACITY, DEFAULT_LOAD_FACTOR);
+	}
+	
+	public MyHashMap(int cap, float loadFactor) {
+		
+		
+	}
 	
 	private int hash(K key) {
 		
@@ -37,4 +69,50 @@ public class MyHashMap {
 		
 	}
 	
+	private boolean equalsValue(V v1, V v2) {
+		
+		
+	}
+	
+	private boolean equalsKey(K k1, K k2) {
+		
+		
+	}
+	
+	public V get(K key) {
+		
+	}
+	
+	public boolean put(K key, V value) {
+		
+		
+	}
+	
+	public boolean remove(K key) {
+		
+		
+	}
+	
+	public boolean containsKey(K key) {
+		
+		
+	}
+	
+	public boolean containsValue(V value) {
+		
+		
+		
+	}
+	
+	public void clear() {
+		
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
 }
